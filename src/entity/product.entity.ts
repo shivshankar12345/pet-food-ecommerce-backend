401f98c7efc0d94f54576bb745e56760c0f0328f
Nilesh Vijay <135734553+Nileshvijay@users.noreply.github.com>
2024-09-23 20:15:12 +0530

    prettier and project setup

--- src/entity/product.entity.ts
+++ src/entity/product.entity.ts
@@ -2,8 +2,7 @@ import {
     Entity,
     PrimaryGeneratedColumn,
     Column,
-    CreateDateColumn,
-    UpdateDateColumn,
+    
   } from 'typeorm';
   
   @Entity('products')
@@ -32,16 +31,13 @@ import {
     @Column({ name: 'brand_id', nullable: true })
     brandId: number;
   
-    @Column({ name: 'seller_id', nullable: true })
+    @Column({ name: 'seller_id'})
     sellerId: number;
   
     @Column({ name: 'pet_type', nullable: true })
     petType: string;
   
-    @CreateDateColumn({ name: 'created_at', type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
-    createdAt: Date;
-  
-    @UpdateDateColumn({ name: 'updated_at', type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
-    updatedAt: Date;
+ 
+    
   }
   
