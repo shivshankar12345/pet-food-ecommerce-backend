c930c4a2b17ac98a021e97035b01c0a491e8cfab
mechlin-sda-shahnawaaz <shahnawaaz.ansari@mechlintech.com>
2024-10-16 18:48:13 +0530

    add Soft Delete feature and make more user more editable by admin

--- src/routes/admin/admin.route.ts
+++ src/routes/admin/admin.route.ts
@@ -1,9 +1,4 @@
 import { NextFunction, Request, Response, Router } from "express";
-import ApplicationError from "../../error/ApplicationError";
-import { AppDataSource } from "../../db/data-source";
-import { Permission } from "../../entity/permission.entity";
-import Responses from "../../modules/responses";
-import { Role } from "../../entity/role.entity";
 import AdminUserManageController from "../../controllers/admin/admin.users.controller";
 import AdminSellerManageController from "../../controllers/admin/admin.seller.controller";
 import AdminRoleManageController from "../../controllers/admin/admin.role.controller";
@@ -15,13 +10,15 @@ const adminSellerController = new AdminSellerManageController();
 const adminRoleController = new AdminRoleManageController();
 const adminPermissionController = new AdminPermissionManageController();
 
-adminRouter.get("/getActiveUsers", adminUserController.getActiveUsers);
-
 adminRouter.get("/getAllUsers", adminUserController.getAllUsers);
 
-adminRouter.get("/getInactiveUsers", adminUserController.getInActiveUsers);
+adminRouter.get("/getUsersByStatus", adminUserController.getUsersByStatus);
 
 adminRouter.patch("/modifyUser", adminUserController.modifyUser);
+
+adminRouter.delete("/deleteUser/:id", adminUserController.deleteUser);
+
+adminRouter.get("/getDeletedUser", adminUserController.getDeletedUsers);
 
 adminRouter.get("/getVerifiedSeller", adminSellerController.getVerifiedSeller);
 
@@ -29,7 +26,7 @@ adminRouter.get("/getPendingSeller", adminSellerController.getPendingSeller);
 
 adminRouter.patch(
   "/changeSellerStatus/approvedSeller",
-  (req, res, next) => {
+  (req: Request, res: Response, next: NextFunction) => {
     req.body.is_verified = true;
     next();
   },
@@ -38,7 +35,7 @@ adminRouter.patch(
 
 adminRouter.patch(
   "/changeSellerStatus/rejectSeller",
-  (req, res, next) => {
+  (req: Request, res: Response, next: NextFunction) => {
     req.body.is_verified = false;
     next();
   },
