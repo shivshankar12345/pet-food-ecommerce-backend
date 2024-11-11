627acc767630b89c2c35f84d301b242a2bae54f8
mechlin-sda-shahnawaaz <shahnawaaz.ansari@mechlintech.com>
2024-11-11 13:57:16 +0530

    Make More APIs documented Secure user Specific Routes and Make Error More Customize

--- src/routes/contact.routes.ts
+++ src/routes/contact.routes.ts
@@ -1,14 +1,31 @@
 import { Router } from "express";
 import ContactController from "../controllers/contact.controller";
+import jwtAuth from "../middlewares/jwtAuth";
+import validateAdmin from "../middlewares/admin.auth";
 
 const contactRouter = Router();
 const contactController = new ContactController();
 
 contactRouter.get("/getAllContact", contactController.getAll);
-contactRouter.post("/addContact", contactController.addContact);
-contactRouter.patch("/updateContact", contactController.updateContact);
+contactRouter.post(
+  "/addContact",
+  jwtAuth,
+  validateAdmin,
+  contactController.addContact
+);
+contactRouter.patch(
+  "/updateContact",
+  jwtAuth,
+  validateAdmin,
+  contactController.updateContact
+);
 contactRouter.post("/connectWithUs", contactController.connectWithUs);
 contactRouter.get("/getSpecificContact", contactController.getContact);
-contactRouter.delete("/deleteContact", contactController.deleteContact);
+contactRouter.delete(
+  "/deleteContact",
+  jwtAuth,
+  validateAdmin,
+  contactController.deleteContact
+);
 
 export default contactRouter;
